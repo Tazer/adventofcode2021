fe255176883511de3b32f878236a89b4da1eca48
Patrik <rambo3swe@gmail.com>
2021-12-03 22:16:24 +0100

    day3 part 2 done

--- day3/main.go
+++ day3/main.go
@@ -22,9 +22,12 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	lines := map[int][]int{}
+	sLines := []string{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		sLines = append(sLines, line)
 
 		for i, l := range line {
 			iL, _ := strconv.Atoi(string(l))
@@ -45,11 +48,27 @@ func main() {
 
 	log.Printf("Result %d", res.Decimal())
 
+	resLife := GetLifeSupport(sLines)
+
+	log.Printf("LIFE SUPPORT %d", resLife.Decimal())
+
 }
 
 type GammaAndEpsilon struct {
 	Gamma   string
 	Epsilon string
+}
+
+type LifeSupportRating struct {
+	OxygenGeneratorRating string
+	CO2ScrubbingRating    string
+}
+
+func (l *LifeSupportRating) Decimal() int64 {
+	oxygen, _ := strconv.ParseInt(l.OxygenGeneratorRating, 2, 64)
+	co2, _ := strconv.ParseInt(l.CO2ScrubbingRating, 2, 64)
+	log.Printf("oxygen %d co2 %d", oxygen, co2)
+	return oxygen * co2
 }
 
 func (g *GammaAndEpsilon) Add(i int) {
@@ -95,4 +114,67 @@ func GetGammaAndEpsilonRate(input map[int][]int) GammaAndEpsilon {
 		i++
 	}
 	return res
+}
+
+func GetLifeSupport(input []string) LifeSupportRating {
+	res := LifeSupportRating{}
+	//Oxygen
+	resList := getLastOne(input, true, 0)
+	log.Printf("Oxygen %v", resList)
+	res.OxygenGeneratorRating = resList[0]
+
+	//Co2
+	resList = getLastOne(input, false, 0)
+	log.Printf("co2 %v", resList)
+	res.CO2ScrubbingRating = resList[0]
+
+	return res
+}
+
+func getLastOne(input []string, most bool, index int) []string {
+
+	if len(input) == 1 {
+		return input
+	}
+
+	ones := 0
+	zero := 0
+
+	for _, v := range input {
+		str := string(v[index])
+		if str == "1" {
+			ones++
+		} else {
+			zero++
+		}
+	}
+
+	allowOnes := false
+
+	if most {
+		if ones > zero || ones == zero {
+			allowOnes = true
+		} else {
+			allowOnes = false
+		}
+	} else {
+		if ones < zero {
+			allowOnes = true
+		} else {
+			allowOnes = false
+		}
+	}
+
+	allowed := []string{}
+
+	for _, v := range input {
+		str := string(v[index])
+		if str == "1" && allowOnes {
+			allowed = append(allowed, v)
+		} else if str == "0" && !allowOnes {
+			allowed = append(allowed, v)
+		}
+	}
+
+	return getLastOne(allowed, most, index+1)
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -41,3 +41,37 @@ func TestGammaAndElips(t *testing.T) {
 
 	assert.Equal(t, int64(198), res.Decimal())
 }
+
+func TestLifesupportRating(t *testing.T) {
+	file, err := os.Open("input_test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	lines := map[int][]int{}
+	sLines := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		sLines = append(sLines, line)
+
+		for i, l := range line {
+			iL, _ := strconv.Atoi(string(l))
+			if lines[i] == nil {
+				lines[i] = []int{}
+			}
+			lines[i] = append(lines[i], iL)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	res := GetLifeSupport(sLines)
+
+	assert.Equal(t, int64(230), res.Decimal())
+}
